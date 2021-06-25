e845f9ce8b0f5d02c3a0251ed7cd2bec0f4311b7
mayumi-kleite <karolyne.leite@etec.sp.gov.br>
2021-06-25 15:35:40 -0300

    migrations testes

---
+++ database/migrations/2021_06_25_152602_create_adresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->integer('numberPlace');
+            $table->string('district');
+            $table->string('city');
+            $table->string('state');
+            $table->string('CEP', 9)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}
